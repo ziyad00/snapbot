1a0bb8030a45d5dc449cbeb949d2b084573ac4e8
ziyad00 <ziyad.alotaibe@gmail.com>
2020-06-18 15:19:35 +0300

    adding downloading videos

--- Slice-MP3.py
+++ Slice-MP3.py
@@ -1,6 +1,10 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext.filters import Filters
+import telebot
+
+
 from telegram import Video
 
 
@@ -29,22 +33,44 @@ def myfunc():
     extract(1)
   else:
     extract(0)
-
+TOEKN = "1092551482:AAGKHtbA_HDKTrTix2rK6_cfKbkk04R9Ys4"
+bot = telebot.TeleBot(TOEKN)
 
 
 def hello(update, context):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-def videoHandler(bot,update):
-  file = bot.getFile(update.message.photo[-1].file_id)
-  print("file_id: " + str(update.message.photo.file_id))
-  file.download('image.jpg')
+def videoHandler(message,update):
+  print("Fffffffffff")
+  raw = message.photo[-1].file_id
+  print(raw)
+  path = raw+".jpg"
+  file_info = bot.get_file(raw)
+  downloaded_file = bot.download_file(file_info.file_path)
+  with open(path,'wb') as new_file:
+    new_file.write(downloaded_file)
+  #filee = bot.getFile(bot.message.photo[-1].file_id)
+  #print(filee.file_path)
+  #filee.download('video.mp4')
 
-updater = Updater('1092551482:AAGKHtbA_HDKTrTix2rK6_cfKbkk04R9Ys4', use_context=True)
+@bot.message_handler(content_types=['video', 'audio'])
+def function_name(message):
+  bot.reply_to(message, "This is a message handler")
+  raw = message.video.file_id
+  print(raw)
+  path = raw+".mp4"
+  file_info = bot.get_file(raw)
+  downloaded_file = bot.download_file(file_info.file_path)
+  with open(path,'wb') as new_file:
+    new_file.write(downloaded_file)
 
-updater.dispatcher.add_handler(MessageHandler(Filters.photo, videoHandler))
-updater.dispatcher.add_handler(CommandHandler("hello", hello))
+bot.polling()
 
-updater.start_polling()
-updater.idle()
+#updater = Updater(TOEKN, use_context=True)
+
+#updater.dispatcher.add_handler(MessageHandler(Filters.photo, videoHandler))
+#updater.dispatcher.add_handler(CommandHandler("hello", hello))
+
+#updater.start_polling()
+#updater.idle()
