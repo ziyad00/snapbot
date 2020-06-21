d58b5ecd884d74415c2981472d46555a29b9dd3c
ziyad00 <ziyad.alotaibe@gmail.com>
2020-06-21 12:30:55 +0300

    dd

--- Slice-MP3.py
+++ Slice-MP3.py
@@ -91,7 +91,7 @@ def getMessage():
 @server.route("/")
 def webhook():
    bot.remove_webhook()
-   bot.set_webhook(url='<HEROKU Web URL>' + TOKEN)
+   bot.set_webhook(url='https://snapbottelegram.herokuapp.com/' + TOKEN)
    return "!", 200
 
 if __name__ == "__main__":
